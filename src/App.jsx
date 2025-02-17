35172e906946f97f50b775db885d620f0b9c9ba9
Hari Prasad Tharu <harip4077@gmail.com>
2025-02-17 21:49:34 +0545

    Router set up

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,11 @@ import CourseDetails from "./page/student/CourseDetails";
 import MyEnrollment from "./page/student/MyEnrollment";
 import Player from "./page/student/Player";
 import Loading from "./components/student/Loading";
+import Educator from "./page/educator/Educator";
+import AddCourse from "./page/educator/AddCourse";
+import Dashboard from "./page/educator/Dashboard";
+import MyCourses from "./page/educator/MyCourses";
+import StudentsEnrolled from "./page/educator/StudentsEnrolled";
 
 function App() {
   return (
@@ -19,6 +24,13 @@ function App() {
         <Route path="/my-enrollments" element={<MyEnrollment />} />
         <Route path="/player/:courseId" element={<Player />} />
         <Route path="/loading/:path" element={<Loading />} />
+
+        <Route path="/educator" element={<Educator />}>
+          <Route path="educator" element={<Dashboard />} />
+          <Route path="add-course" element={<AddCourse />} />
+          <Route path="My-courses" element={<MyCourses />} />
+          <Route path="students-enrolled" element={<StudentsEnrolled />} />
+        </Route>
       </Routes>
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,6 @@ createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <AppContextProvider>
     <App />
-  </AppContextProvider>,
+  </AppContextProvider>
   </BrowserRouter>
 )
