3b4c5c11b1bee7bc212a1d0319010195f941b497
Hari Prasad Tharu <harip4077@gmail.com>
2025-02-17 16:50:13 +0545

    Router set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-
-import React from 'react'
-
-import './App.css'
+import React from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./page/student/Home";
+import CourseList from "./page/student/CourseList";
+import CourseDetails from "./page/student/CourseDetails";
+import MyEnrollment from "./page/student/MyEnrollment";
+import Player from "./page/student/Player";
+import Loading from "./components/student/Loading";
 
 function App() {
   return (
     <div>
-      <h1 className=' text-amber-600'>React App</h1>
-      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/course-list" element={<CourseList />} />
+        <Route path="/courselist:input" element={<CourseList />} />
+        <Route path="/coursedetail/:id" element={<CourseDetails />} />
+        <Route path="/my-enrollments" element={<MyEnrollment />} />
+        <Route path="/player/:courseId" element={<Player />} />
+        <Route path="/loading/:path" element={<Loading />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
